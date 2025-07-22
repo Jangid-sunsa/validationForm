ce752611893348eacdacc0d47b040693b1f63cef
Sunil Jangid <sutharsunil7742@gmail.com>
2025-07-22 19:25:52 +0530

    Update validation_forms.js

--- Js-validation-forms/validation_forms.js
+++ Js-validation-forms/validation_forms.js
@@ -17,6 +17,179 @@ function resetErrors() {
   document.getElementById("checkbox-error").textContent = "";
 }
 
+function addRealTimeValidation() {
+  const nameInput = document.getElementById("fname");
+  const emailInput = document.getElementById("femail");
+  const phoneInput = document.getElementById("fphone");
+  const dobInput = document.getElementById("fdob");
+  const genderInput = document.getElementById("fgender");
+  const cityInput = document.getElementById("fcity");
+  const stateInput = document.getElementById("fstate");
+  const countryInput = document.getElementById("fcountry");
+  const addressInput = document.getElementById("faddress");
+  const messageInput = document.getElementById("fmssg");
+  const radioYesInput = document.getElementById("fyes");
+  const radioNoInput = document.getElementById("fno");
+  const fileInput = document.getElementById("file");
+  const checkboxInput = document.getElementById("checkbox");
+
+  nameInput.addEventListener("input", () => {
+    const nameErr = document.getElementById("name-error");
+    const val = nameInput.value;
+    if (val === "" || /[\d!@#$%^&*(),.?":{}|<>_\-+=\\\/~`[\];']/g.test(val)) {
+      nameErr.textContent = "Please enter your name properly.";
+    } else {
+      nameErr.textContent = "";
+    }
+  });
+
+  emailInput.addEventListener("input", () => {
+    const emailErr = document.getElementById("email-error");
+    const val = emailInput.value;
+    if (val === "") {
+      emailErr.textContent = "Please enter your email.";
+    } else if (!val.includes("@") || !val.includes(".")) {
+      emailErr.textContent = "Please enter your valid email.";
+    } else {
+      emailErr.textContent = "";
+    }
+  });
+
+  phoneInput.addEventListener("input", () => {
+    const phoneErr = document.getElementById("phone-error");
+    const val = phoneInput.value;
+    if (val === "") {
+      phoneErr.textContent = "Please enter your phone no.";
+    } else if (val.length < 10) {
+      phoneErr.textContent = "Please enter your valid phone no.";
+    } else {
+      phoneErr.textContent = "";
+    }
+  });
+
+  dobInput.addEventListener("change", () => {
+    const dobErr = document.getElementById("dob-error");
+    if (!dobInput.value) {
+      dobErr.textContent = "Date of birth is Required.";
+    } else {
+      dobErr.textContent = "";
+    }
+  });
+
+  const genderErr = document.getElementById("gender-error");
+
+  genderInput.addEventListener("input", () => {
+    const genderVal = genderInput.value.trim().toLowerCase();
+    if (
+      genderVal === "male" ||
+      genderVal === "female" ||
+      genderVal === "other"
+    ) {
+      genderErr.textContent = "";
+    } else if (genderInput.value === "") {
+      genderErr.textContent = "";
+    } else {
+      genderErr.textContent = "Please select Male, Female, or Other.";
+    }
+  });
+
+  cityInput.addEventListener("input", () => {
+    const cityErr = document.getElementById("city-error");
+    const val = cityInput.value;
+    if (val === "") {
+      cityErr.textContent = "Enter your city.";
+    } else if (/\d/.test(val)) {
+      cityErr.textContent = "Enter a valid city.";
+    } else {
+      cityErr.textContent = "";
+    }
+  });
+
+  stateInput.addEventListener("input", () => {
+    const stateErr = document.getElementById("state-error");
+    const val = stateInput.value;
+    if (val === "") {
+      stateErr.textContent = "Enter your state.";
+    } else if (/\d/.test(val)) {
+      stateErr.textContent = "Enter a valid state.";
+    } else {
+      stateErr.textContent = "";
+    }
+  });
+
+  countryInput.addEventListener("input", () => {
+    const countryErr = document.getElementById("country-error");
+    const val = countryInput.value;
+    if (val === "") {
+      countryErr.textContent = "Enter your country.";
+    } else if (/\d/.test(val)) {
+      countryErr.textContent = "Enter a valid country.";
+    } else {
+      countryErr.textContent = "";
+    }
+  });
+
+  addressInput.addEventListener("input", () => {
+    const addressErr = document.getElementById("address-error");
+    if (addressInput.value === "") {
+      addressErr.textContent = "Enter your address.";
+    } else {
+      addressErr.textContent = "";
+    }
+  });
+
+  messageInput.addEventListener("input", () => {
+    const messageErr = document.getElementById("message-error");
+    if (messageInput.value === "" || addressInput.value.length > 250) {
+      messageErr.textContent = "Enter your message.";
+    } else {
+      messageErr.textContent = "";
+    }
+  });
+
+  function validateRadios() {
+    const radioYesErr = document.getElementById("radioY-error");
+    const radioNoErr = document.getElementById("radioN-error");
+    if (!radioYesInput.checked && !radioNoInput.checked) {
+      radioNoErr.textContent = "Choose one.";
+    } else {
+      radioNoErr.textContent = "";
+      radioYesErr.textContent = "";
+    }
+  }
+
+  radioYesInput.addEventListener("change", validateRadios);
+  radioNoInput.addEventListener("change", validateRadios);
+
+  fileInput.addEventListener("change", () => {
+    const fileErr = document.getElementById("file-error");
+    if (fileInput.files.length === 0) {
+      fileErr.textContent = "Upload file mandatory.";
+      return;
+    }
+    const file = fileInput.files[0];
+    const size = file.size / 1024 / 1024;
+    const fileName = file.name;
+    const allowedExtensions = /(\.pdf)$/i;
+    if (!allowedExtensions.exec(fileName)) {
+      fileErr.textContent = "Invalid file type. Only .pdf allowed.";
+    } else if (size > 2) {
+      fileErr.textContent = "File size should be less than 2 MB.";
+    } else {
+      fileErr.textContent = "";
+    }
+  });
+
+  checkboxInput.addEventListener("change", () => {
+    const checkErr = document.getElementById("checkbox-error");
+    if (!checkboxInput.checked) {
+      checkErr.textContent = "Checked compulsorry";
+    } else {
+      checkErr.textContent = "";
+    }
+  });
+}
+
 function formsValidation() {
   const name = document.getElementById("fname").value;
   const email = document.getElementById("femail").value;
@@ -181,7 +354,7 @@ function formsValidation() {
     if (editIndex !== null) {
       storedData[editIndex] = formData;
 
-      alert("Form updated successfully"); 
+      alert("Form updated successfully");
       editIndex = null;
     } else {
       storedData.push(formData);
@@ -211,18 +384,34 @@ function displayData() {
 
     row.innerHTML = `
     <td class="text-center border border-black p-1">${index + 1}</td>
-      <td class="text-center border border-black p-1">${item.Name}</td>
-      <td class="text-center border border-black p-1">${item.Email}</td>
-      <td class="text-center border border-black p-1">${item.Phone}</td>
-      <td class="text-center border border-black p-1">${item.DOB}</td>
-      <td class="text-center border border-black p-1">${item.Gender}</td>
-      <td class="text-center border border-black p-1">${item.City}</td>
-      <td class="text-center border border-black p-1">${item.State}</td>
-      <td class="text-center border border-black p-1">${item.Country}</td>
-      <td class="text-center border border-black p-1">${item.Address}</td>
-      <td class="text-center border border-black p-1">${item.Message}</td>
-      <td class="text-center border border-black p-1">${item.FileName}</td>
-      <td class="text-center border border-black p-1">
+      <td class="capitalize text-start border border-black p-1">${
+        item.Name
+      }</td>
+      <td class="text-start border border-black p-1">${item.Email}</td>
+      <td class="text-start border border-black p-1">${item.Phone}</td>
+      <td class="text-start border border-black p-1">${item.DOB}</td>
+      <td class="capitalize text-start border border-black p-1">${
+        item.Gender
+      }</td>
+      <td class="capitalize text-start border border-black p-1">${
+        item.City
+      }</td>
+      <td class="capitalize text-start border border-black p-1">${
+        item.State
+      }</td>
+      <td class="capitalize text-start border border-black p-1">${
+        item.Country
+      }</td>
+      <td class="capitalize text-start border border-black p-1">${
+        item.Address
+      }</td>
+      <td class="capitalize text-start border border-black p-1">${
+        item.Message
+      }</td>
+      <td class="capitalize text-start border border-black p-1">${
+        item.FileName
+      }</td>
+      <td class="text-start border border-black p-1">
         <button type="button" onclick="editRow(${index})"><i class="fa-solid fa-pen-to-square text-2xl px-2 active:text-blue-600 cursor-pointer"></i></button>
         <button type="button" onclick="deleteRow(${index})"><i class="fa-solid fa-trash text-2xl px-2 active:text-red-600 cursor-pointer"></i></button>
       </td>
@@ -246,6 +435,9 @@ function editRow(index) {
   document.getElementById("faddress").value = data.Address;
   document.getElementById("fmssg").value = data.Message;
   document.getElementById("file").file = data.FileInput;
+  document.getElementById("fyes").checked = data.Counseling === "Yes";
+  document.getElementById("fno").checked = data.Counseling === "No";
+  document.getElementById("checkbox").checked = data.Checked;
 
   editIndex = index;
 }
@@ -259,4 +451,5 @@ function deleteRow(index) {
 
 window.onload = function () {
   displayData();
+  addRealTimeValidation();
 };
